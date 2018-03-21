8c85d98069aed02c20b579ea4c838006fec92b74
Alex Huszagh <ahuszagh@ahuszagh.localdomain>
2018-03-20 19:03:37 -0700

    First commit.

---
+++ handler.py
@@ -0,0 +1,50 @@
+__copyright__ = "2018 QChain Inc. All Rights Reserved."
+__contact__ = "https://qchain.co/licensing/"
+__license__ = "GNU AGPLv3 or commercial, see LICENSE."
+
+'''
+    handler
+    -------
+
+    Handler code for testnet XQC functions.
+'''
+
+import json
+import testnet
+
+
+# FUNCTIONS
+# ---------
+
+
+def request_error(exception):
+    '''Format an internal server error to a diagnostic message.'''
+
+    return {
+        "statusCode": 500,
+        "body": json.dumps({
+            "message": "Internal server error.",
+            "error": type(exception).__name__
+        })
+    }
+
+
+def send_testnet_xqc(event, context):
+    '''Handler to send testnet XQC to a custom address.'''
+
+    parameters = event['queryStringParameters']
+    try:
+        address = parameters['address']
+        amount = parameters.get('amount', testnet.CONFIG['default_transfer_amount'])
+        node_list = parameters.get('nodeList', testnet.CONFIG['node_list'])
+        body = testnet.send_xqc(address, amount, node_list)
+
+    except Exception as exception:
+        return request_error(exception)
+
+    response = {
+        "statusCode": 200,
+        "body": body.to_json()
+    }
+
+    return response
---
+++ testnet.py
@@ -0,0 +1,115 @@
+__copyright__ = "2018 QChain Inc. All Rights Reserved."
+__contact__ = "https://qchain.co/licensing/"
+__license__ = "GNU AGPLv3 or commercial, see LICENSE."
+
+'''
+    testnet
+    -------
+
+    Testnet handler functions.
+'''
+
+import binascii
+import datetime
+import decimal
+import json
+import math
+from qchain import common, nem
+
+# CONFIG
+# ------
+
+with open('config.json') as f:
+    CONFIG = json.load(f)
+
+MOSAIC_DEFINITION = nem.MosaicDefinition.from_dict(CONFIG['mosaic'])
+MESSAGE = nem.Message.from_dict(CONFIG['message'])
+
+# FUNCTIONS
+# ---------
+
+def estimate_fee(mosaic):
+    '''
+    Over-estimate the fee required for a transaction.
+    Adapted from:
+        https://nemproject.github.io/#transaction-fees
+        https://blog.nem.io/nem-updated-0-6-82/
+
+    :param *amounts: Sequence of microXEM (or XEM, etc.) to send.
+    '''
+
+    mosaic_fee = nem.calculate_mosaic_fee(MOSAIC_DEFINITION, mosaic)
+    message_fee = nem.calculate_message_fee(MESSAGE)
+
+    return mosaic_fee + message_fee
+
+
+def create_transfer(recipient, amount):
+    '''
+    Create the transfer transaction (v2) with the mosaics.
+
+    :param recipient: Address to which XQC will be sent.
+    :param amount: Number of XQC to send.
+    '''
+
+    time_stamp = nem.new_time_stamp()
+    mosaic = nem.Mosaic(MOSAIC_DEFINITION.mosaic_id, int(amount))
+
+    return nem.TransferTransaction(
+        time_stamp=time_stamp,
+        signature='',
+        fee=estimate_fee(mosaic),
+        type=nem.Transfer,
+        deadline=time_stamp + datetime.timedelta(hours=1).seconds,
+        version=nem.TestNetworkVersion,
+        signer=CONFIG['public_key'],
+        relative_version=2,
+        amount=0,
+        recipient=recipient,
+        message=MESSAGE,
+        mosaics=[mosaic]
+    )
+
+
+def send_xqc(recipient, amount, node_list):
+    '''
+    Send testnet XQC to a NEM address. The sending address is hard-coded
+    to a master sending account.
+
+    :param recipient: Address to which XQC will be sent.
+    :param amount: Number of XQC to send.
+    :param node_list: List of IP addresses for NIS nodes.
+    :return: NEM response
+    '''
+
+    # get parameters
+    amount = nem.MicroXem(nem.Xem(amount))
+    transfer = create_transfer(recipient, amount)
+    public_key = common.SecureString.from_file('key.pub')
+    private_key = common.SecureString.from_file('key')
+
+    # get raw byte arrays
+    raw_transaction = transfer.to_bytes()
+    raw_signature = nem.sign(raw_transaction, public_key,  private_key)
+    transaction = binascii.hexlify(raw_transaction).decode('ascii')
+    signature = binascii.hexlify(raw_signature).decode('ascii')
+    request = nem.RequestAnnounce(transaction, signature)
+    result = nem.Error(transfer.time_stamp, "Bad Request", None, 400)
+
+    for node in node_list:
+        client = nem.Client(*node)
+        hb = client.heartbeat()
+        if isinstance(hb, nem.NemRequestResult) and hb.code == 1:
+            # request ok, make message
+            result = client.transaction.announce(request)
+            if not isinstance(request, nem.Error):
+                # successful posted the result, return
+                return result
+            elif result.message == 'FAILURE_INSUFFICIENT_FEE':
+                # insufficient fee provided
+                return result
+            elif result.message == 'FAILURE_INSUFFICIENT_BALANCE':
+                # account has insufficient balance for the transaction
+                return result
+
+    return result
